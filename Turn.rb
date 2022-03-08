3304c019b3e07dab01ae2cb80307c169e7693658
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-03-07 19:44:32 -0500

    math game using ruby

---
+++ Turn.rb
@@ -0,0 +1,25 @@
+class Turn 
+  attr_accessor :player
+
+  def initialize(player, question)
+    @player = player
+    @question = question
+  end
+
+  def ask_question
+    puts "#{@player.name}: #{@question.question}"
+  end
+
+  def check_answer
+    print "> "
+    user_answer = gets.chomp.to_i
+
+    if(@question.answer == user_answer)
+      puts "YES! You are correct."
+    else
+      @player.score -= 1
+      puts "Seriously? No!"
+    end
+  end
+
+end
